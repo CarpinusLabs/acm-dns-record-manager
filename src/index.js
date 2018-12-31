ac404161055e3a1976246c4c48be30bcb93843af
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2018-12-31 09:51:41 +0100

    Support for deleting resource records
    ACM recource records are deleted when the corresponding certificate has been deleted.

--- src/index.js
+++ src/index.js
@@ -14,23 +14,57 @@ exports.handler = async (event) => {
 exports.processEvent = async (event) => {
   let message = parseMessage(event.Sns.Message);
 
-  if (message.ResourceStatus === 'CREATE_IN_PROGRESS' && message.ResourceType === 'AWS::CertificateManager::Certificate' && message.PhysicalResourceId !== undefined) {
-    const certificate_arn = message.PhysicalResourceId;
+  if (message.ResourceType !== 'AWS::CertificateManager::Certificate') {
+    return;
+  }
 
-    console.log(`Getting Hosted Zone ID for certificate ${certificate_arn}...`);
-    const hostedZoneId = await getHostedZoneIdForCertificate(certificate_arn);
-    if (hostedZoneId === null) {
-      console.log(`Failed to get Hosted Zone ID for certificate ${certificate_arn}. Make sure to assign it by adding the tag "HostedZoneID".`);
-      return;
-    }
-
-    console.log(`Getting resource records for certificate ${certificate_arn}...`);
-    const resource_records = await getResourceRecordsForCertificate(certificate_arn);
-
-    console.log(`Creating resource records for certificate ${certificate_arn}...`);
-    await createCNAMERecord(resource_records, hostedZoneId);
+  if (message.ResourceStatus === 'CREATE_IN_PROGRESS' && message.PhysicalResourceId !== undefined) {
+    await processCreateInProgressMessage(message);
+  } else if (message.ResourceStatus === 'DELETE_COMPLETE') {
+    await processDeleteCompleteMessage(message);
   }
 };
+
+async function processCreateInProgressMessage(message) {
+  const certificateArn = message.PhysicalResourceId;
+
+  console.log(`Getting Hosted Zone ID for certificate ${certificateArn}...`);
+  const hostedZoneId = await getHostedZoneIdForCertificate(certificateArn);
+  if (hostedZoneId === null) {
+    console.log(`Failed to get Hosted Zone ID for certificate ${certificateArn}. Make sure to assign it by adding the tag "HostedZoneID".`);
+    return;
+  }
+
+  console.log(`Getting resource records for certificate ${certificateArn}...`);
+  const resourceRecords = await getResourceRecordsForCertificate(certificateArn);
+
+  console.log(`Creating resource records for certificate ${certificateArn}...`);
+  await createCNAMERecord(resourceRecords, hostedZoneId);
+}
+
+async function processDeleteCompleteMessage(message) {
+  console.log(`Certificate ${message.PhysicalResourceId} has been deleted`);
+  const properties = JSON.parse(message.ResourceProperties);
+
+  const hostedZoneId = getHostedZoneIdFromTags(properties.Tags);
+  if (hostedZoneId === null) {
+    return;
+  }
+
+  let domainNames = [ properties.DomainName ];
+  if (properties.SubjectAlternativeNames) {
+    domainNames = domainNames.concat(properties.SubjectAlternativeNames);
+  }
+
+  const acmRecords = await getACMRecords(hostedZoneId);
+  if (acmRecords.length === 0) {
+    return;
+  }
+
+  const resourceRecords = getResourceRecordsForDomains(acmRecords, domainNames);
+
+  await deleteCNAMERecord(resourceRecords, hostedZoneId);
+}
 
 async function getHostedZoneIdForCertificate(arn) {
   const params = {
@@ -39,16 +73,19 @@ async function getHostedZoneIdForCertificate(arn) {
 
   try {
     const result = await acm.listTagsForCertificate(params).promise();
-
-    const tags = result.Tags.filter((tag) => {
-      return (tag.Key === 'HostedZoneId');
-    });
-
-    return tags.length === 0 ? null : tags[0].Value;
+    return getHostedZoneIdFromTags(result.Tags);
   } catch (error) {
     console.log('Failed to get tags for certificate ' + arn);
     throw error;
   }
+}
+
+function getHostedZoneIdFromTags(tags) {
+  const filteredTags = tags.filter((tag) => {
+    return (tag.Key === 'HostedZoneId');
+  });
+
+  return filteredTags.length === 0 ? null : filteredTags[0].Value;
 }
 
 async function getResourceRecordsForCertificate(arn) {
@@ -111,6 +148,79 @@ async function createCNAMERecord(records, hostedZoneId) {
     console.log('Failed to create Route 53 record set: ' + error);
     throw error;
   }
+}
+
+async function deleteCNAMERecord(records, hostedZoneId) {
+  const changes = records.map((record) => {
+    return {
+      Action: 'DELETE',
+      ResourceRecordSet: {
+        Name: record.Name,
+        ResourceRecords: [
+          {
+            Value: record.Value
+          }
+        ],
+        TTL: 300,
+        Type: record.Type
+      }
+    };
+  });
+
+  const params = {
+    ChangeBatch: {
+      Changes: changes
+    },
+    HostedZoneId: hostedZoneId
+  };
+
+  try {
+    await route53.changeResourceRecordSets(params).promise();
+  } catch (error) {
+    console.log('Failed to delete Route 53 record set: ' + error);
+    throw error;
+  }
+}
+
+async function getACMRecords(hostedZoneId) {
+  const params = {
+    HostedZoneId: hostedZoneId
+  };
+
+  try {
+    const result = await route53.listResourceRecordSets(params).promise();
+    return result.ResourceRecordSets.filter((record) => {
+      if (record.Type !== 'CNAME') {
+        return false;
+      }
+
+      let acmRecords = record.ResourceRecords.filter((item) => {
+        return item.Value.endsWith('.acm-validations.aws.');
+      });
+      return acmRecords.length > 0;
+    });
+  } catch (error) {
+    console.log('Failed to list resource records of hosted zone "' + hostedZoneId + '": ' + error);
+    throw error;
+  }
+}
+
+function getDomainNameForACMRecord(record) {
+  const regexp = /_[^\.]+\.(.+)\./;
+  const result = regexp.exec(record.Name);
+  return result[1];
+}
+
+function getResourceRecordsForDomains(records, domains) {
+  return records.filter((record) => {
+    return domains.includes(getDomainNameForACMRecord(record));
+  }).map((record) => {
+    return {
+      Name: record.Name,
+      Type: record.Type,
+      Value: record.ResourceRecords[0].Value
+    };
+  });
 }
 
 function parseMessage(message) {
